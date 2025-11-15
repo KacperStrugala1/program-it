19fbba76afedb4486cdbc9279b10f6f82dab71a5
KacperStrugala1 <kacperstrugala5@gmail.com>
2025-11-15 08:51:53 +0100

    Updated profile edit area

--- programit/main/admin.py
+++ programit/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models.time_model import Time
+from .models.profile_model import ProfileModel
 
 
 # Register your models here.
 admin.site.register(Time)
-
+admin.site.register(ProfileModel)

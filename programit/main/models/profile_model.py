bb7e11c099eac7fd0766d35470bfd995da458d0c
kstrugal <kacperstrugala5@gmail.com>
2025-11-15 21:55:52 +0100

    Added new gui

--- programit/main/models/profile_model.py
+++ programit/main/models/profile_model.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class ProfileModel(models.Model):
     username = models.CharField(max_length=150, unique=True)
-    github_username = models.CharField(max_length=150, unique=True)
+    avatar_url = models.ImageField('img', upload_to='path/', blank=True, null=True)
+    github_username = models.CharField(max_length=150, unique=True, null=True)
     description = models.TextField(blank=True)
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE, default=None)
+    points = models.IntegerField(default=0)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

a428d8a1f4a9e72830524fdef228033766b9eeb4
KacperStrugala1 <kacperstrugala5@gmail.com>
2025-11-20 23:24:40 +0100

    added new model to panel

--- programit/main/admin.py
+++ programit/main/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models.time_model import Time
 from .models.profile_model import ProfileModel
+from .models.backlog_model import Backlog
 
 
 # Register your models here.
 admin.site.register(Time)
 admin.site.register(ProfileModel)
+admin.site.register(Backlog)
